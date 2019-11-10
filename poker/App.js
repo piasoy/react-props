1ea2d0feb63ab4569b83cdc809eac26d73b4c124
Pia Soy <piasoy4@gmail.com>
2019-11-10 09:50:54 -0600

    complete poker exercise

--- poker/App.js
+++ poker/App.js
@@ -1,11 +1,29 @@
 // Render the cards using React!
 const App = (props) => {
     let { cards } = props;
+    //console.log(props)
+    let Cards = cards.map(Card)
     return (
-        <span>cards</span>
+        <div>{Cards}</div>
     )
 }
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+//const Card = null;  
+const Card = (props, index) => {
+ 
+    let {value, suit} = props
+    let url = "cards/" + value + suit + ".png"
+    let divStyle = {
+        display: 'inline'
+    }
+    let imageStyle = {
+        width: '100px'     
+    }
+    
+    return (
+        <div key={index} style={divStyle}><img style={imageStyle} src={url}></img></div>
+    )
+
+}
