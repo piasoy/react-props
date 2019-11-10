833b2a11ff90c392c323316cfe8fa9305d0c8e9d
Pia Soy <piasoy4@gmail.com>
2019-11-09 23:15:33 -0600

    complete circles exercise

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,30 @@
 // Render the circles using React!
 const App = (props) => {
     let { circles } = props;
+    //console.log('App component props:', props)
+    //circles property is an array so to render it, you have to map through it
+    const Circles = circles.map(Circle)
     return (
-        <span>circles</span>
+        <span>{Circles}</span>
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+//const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+const Circle = (props) => {
+     //console.log('Circle component props:', props) 
+     //Circle property is radius & color
+    //create style attribute value that is an object
+    let { color, radius } = props;
+    const circleStyle = {
+        backgroundColor : color,
+        height : radius * 2,
+        width : radius * 2,
+        borderRadius: '50%'
+    }
+
+    //style attribute accepts JS object instead of string
+    // ? why is in it curly bracket?
+     return (
+         <div style={circleStyle}></div>
+     )
+}
