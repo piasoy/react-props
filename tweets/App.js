2b6118cdaf19b2f805964b85a200a534f616b61b
Pia Soy <piasoy4@gmail.com>
2019-11-10 14:56:06 -0600

    finish twitter exercise

--- tweets/App.js
+++ tweets/App.js
@@ -1,15 +1,109 @@
 // Render the tweets using React!
 const App = (props) => {
     let { tweets } = props;
+    //console.log(props)
+   let Tweets = tweets.map(Tweet)
+
     return (
-        <span>tweets</span>
+        <div>{Tweets}</div>
     )
 }
 
-const Tweet = null;  // CHALLENGE: Write a separate Tweet component for use in the App component
+//const Tweet = null;  // CHALLENGE: Write a separate Tweet component for use in the App component
+const Tweet = (props) => {
+    //console.log(props)
+    let {user, text, likes, retweets, replies} = props
+    let TweetDivStyle = {
+        marginTop:'50px'
+    }
+    return (<div style={TweetDivStyle}>
+                <User user={user} />
+                <h2>{text}</h2>
+                <Metrics props={props} />
+            </div>)
+}
+//const User = null;  // CHALLENGE: Write a separate User component for use in the Tweet component
+const User = (props) => {
+    console.log(props)
+    let { user } = props
+    let UserOuterDivStyle = {
+        display:'flex',
+        alignItems:'flex-start'
+    }
+    let UserProfileDivStyle = {marginLeft: '20px'}
+    let UserNameOuterDivStyle = {
+        display: 'flex',
+        alignItems: 'flex-start'
+    }
+    let UserNameStyle = {
+        marginBottom: '5px',
+        marginRight: '5px'
+    }
+    let CheckStyle = {width: '15%'}
+    let UserAvatarStyle = {width: '5%'}
+    let checkImg = "twitterIcons/check-circle.svg"
 
-const User = null;  // CHALLENGE: Write a separate User component for use in the Tweet component
+    const isVerified = (user) => {
+        console.log('isVerified', user.isVerified)
+        return (user.isVerified ? checkImg : '')
+    }
+   
+    return (<div style={UserOuterDivStyle}>
+                <img style={UserAvatarStyle} src={user.profilePic}/> 
+                <div style={UserProfileDivStyle}>
+                    <div style={UserNameOuterDivStyle}>
+                        <p style={UserNameStyle}>{user.username}</p>
+                        <img style={CheckStyle} src={isVerified(user)}/> 
+                    </div>
+                    <p>{user.handle}</p>
+                </div>              
+            </div>
+            )
+}
+//const Metrics = null;  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
+const Metrics = (props) => {
+    //console.log(props)
+    let MetricsOuterDivStyle = {
+        display: 'flex',
+        flowDirection: 'row',
+        alignItems: 'flex-start'
+    }
+    let MetricsInnerDivStyle = {
+        display: 'flex',
+        width: '10%'
+    }
 
-const Metrics = null;  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
+    let ImgStyle = {
+        width: '27%',
+        marginRight: '7px'
+    }
 
+    let ParagraphStyle = {
+        marginTop:'7px',
+        marginBottom: '0px'
+    }
+
+    let commentImg = "twitterIcons/message-circle.svg"
+    let retweetImg = "twitterIcons/repeat.svg"
+    let likeImg = "twitterIcons/heart.svg"
+    let msgImg = "twitterIcons/message-circle.svg"
+    return (<div style={MetricsOuterDivStyle}>
+                 <div style={MetricsInnerDivStyle}>
+                    <img style={ImgStyle} src={commentImg}/> 
+                    <p style={ParagraphStyle}>{props.replies}K</p>
+                 </div>
+                 <div style={MetricsInnerDivStyle}>
+                    <img style={ImgStyle} src={retweetImg}/> 
+                    <p style={ParagraphStyle}>{props.retweets}K</p>
+                 </div>
+                 <div style={MetricsInnerDivStyle}>
+                    <img style={ImgStyle} src={likeImg}/> 
+                    <p style={ParagraphStyle}>{props.likes}K</p>
+                 </div>
+                 <div style={MetricsInnerDivStyle}>
+                    <img style={ImgStyle} src={msgImg}/> 
+                 </div>
+            </div>
+    )
+}
 // BONUS CHALLENGE - Delete your Tweet component code and rewrite the App component so that it uses User and Metrics directly
